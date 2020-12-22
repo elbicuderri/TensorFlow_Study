e1022dcfcc1bd828a5d1193e5f2812279b6098cd
elbicuderri <muger1031@gmail.com>
2020-12-22 22:31:50 +0900

    einsum

---
+++ einsum/einsum.py
@@ -0,0 +1,31 @@
+import tensorflow as tf 
+# from tensorflow.random import uniform 
+
+# matmul
+x = tf.random.uniform([2, 3])
+y = tf.random.uniform([4, 3])
+
+z = tf.einsum("ij, kj->ik", x, y)
+
+print(z)
+
+#Fully Connected Layer
+a = tf.random.uniform([32, 3, 228, 228])
+
+b = tf.random.uniform([32, 228, 228, 3])
+
+w1 = tf.random.uniform([10, 3 * 228 * 228])
+
+w2 = tf.random.uniform([228 * 228 * 3, 10])
+
+y1 = tf.einsum("nchw, kchw-> nk", a, w1.numpy().reshape([10, 3, 228, 228])) #PyTorch
+
+y2 = tf.einsum("nhwc, hwck-> nk", b, w2.numpy().reshape([228, 228, 3, 10])) #TensorFlow
+
+print(y1.shape)
+
+print(y2.shape)
+
+print(y1)
+
+print(y2)
